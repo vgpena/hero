31641684c301604b1f670022c2feb7bcf82cea92
Violet Peña <vpena@oberlin.edu>
2012-06-29 15:15:20 -0400

    Made the Stories navigation script more abstract and restyles the Stories and Photos tabs accordingly.

---
+++ hero/js/nav_intra.js
@@ -0,0 +1,64 @@
+(function($){
+	
+	var IntraNav = function (section) {
+		var that = this;
+		that.build(section);
+	};
+	
+	IntraNav.prototype = {
+		
+		constructor: IntraNav,
+		
+		build: function (section) {
+			var content = $('.content', section),
+				thumbs = $('.thumbs', section);
+			$('figure', thumbs).each(function(){
+				var id = $(this).attr('id'),
+					thumb = $(this);
+				$('article', section).each(function(){
+					if ($(this).attr('id')==id){
+						var elt = $(this);
+						thumb.on('click', elt, function(){
+							
+							$('article', 'section').each(function(){
+								if ($(this).hasClass('opened')){
+									$(this).removeClass('opened');
+									$(this).hide();
+								};
+							});
+							
+							elt.fadeIn(800);
+							elt.addClass('opened');
+							$('figure', thumbs).each(function(){
+								if ($(this).hasClass('opened')){
+									$(this).removeClass('opened');
+								};
+							});
+							thumb.addClass('opened');
+						});
+					};
+				});
+			});
+		},
+	};
+
+	$.fn.intraNav = function (arg) {
+		return this.each(function(){
+			var $this = $(this),
+				data = $this.data('intraNav');
+			
+			if (data) {
+				data[arg]();
+			}else{
+				$this.data('intraNav', new IntraNav($this));
+			};
+		});
+	};
+	
+
+	$(function(){
+		$('#stories').intraNav();
+		$('#photos').intraNav();
+	});
+	
+}(jQuery));
